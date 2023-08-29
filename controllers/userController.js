904f815ddc6c6eff93b75000f7c424e71cc2083a
Charles2205 <cewudzie0@gmail.com>
2023-08-29 11:40:30 +0000

    started project

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const home =(req,res)=>{
+res.send('/pages/home')
+}
+const about =(req,res)=>{
+ res.send('/pages/about')   
+}
+const courses =(req,res)=>{
+res.send('/pages/courses')    
+}
+const trainers =(req,res)=>{
+ res.send('/pages/trainers')   
+}
+const events =(req,res)=>{
+  res.send('/pages/events')  
+}
+const pricing =(req,res)=>{
+ res.send('/pages/pricing')   
+}
+const dropdown =(req,res)=>{
+ res.send('/pages/dropdown')   
+}
+const contact =(req,res)=>{
+  res.send('/pages/home')  
+}
+const getStarted =(req,res)=>{
+   res.send('/pages/getstarted') 
+}
